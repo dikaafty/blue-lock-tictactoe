494d863d5e87ae0d61bb7e345bcb2eb670a92576
Dika Afty <dikaafty@gmail.com>
2025-01-23 14:37:29 +0700

    Declare winning combinations

--- main.js
+++ main.js
@@ -3,6 +3,22 @@
   const gameboard = (() => {
     let board = ["", "", "", "", "", "", "", "", ""];
 
+    const winningCombinations = [
+      // Rows
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+
+      // Columns
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 9],
+
+      // Diagonals
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
     const getBoard = () => {
       return board;
     };
